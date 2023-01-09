6c50fc7106850aaaf6b7da309c53c191400c1548
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-01-09 09:06:09 -0500

    Edited Files

--- USACO/Gold/2017-2018/February/Snow Boots.cpp
+++ USACO/Gold/2017-2018/February/Snow Boots.cpp
@@ -1,62 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define nl "\n"
-#define spc " "
-#define sz(x) ((int) x.size())
 
 int main() {
+	freopen("snowboots.in", "r", stdin);
+	freopen("snowboots.out", "w", stdout);
+
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	freopen("snowboots.in", "r", stdin);
-	freopen("snowboots.out", "w", stdout);
-	
 	int N, B;
 	cin >> N >> B;
 
-	vector<int> tiles(N);
-	set<int> indices;
-	vector<int> indices_by_depth;
+	set<int> tiles;
+	priority_queue<pair<int, int>> depths;
 
 	for (int i = 0; i < N; i++) {
-		cin >> tiles[i];
+		int depth; cin >> depth;
+		tiles.insert(i); depths.push({depth, i});
 	}
 
-	for (int i = 0; i < N; i++) {
-		indices.insert(i);
-		indices_by_depth.push_back(i);
-	}
-
-	sort(indices_by_depth.begin(), indices_by_depth.end(), [&](int x, int y) { return tiles[x] > tiles[y]; });
-
-	vector<array<int, 3>> snowboots(B);
+	priority_queue<array<int, 3>> boots;
 	for (int i = 0; i < B; i++) {
-		cin >> snowboots[i][0] >> snowboots[i][1];
-		snowboots[i][2] = i;
+		int depth, step; cin >> depth >> step;
+		boots.push({depth, step, i});
 	}
 
-	sort(snowboots.rbegin(), snowboots.rend());
+	int max_step = 1;
+	vector<bool> viable(B);
 
-	vector<bool> answer_key(B);
-	int tile_depth_index = 0, max_gap = 1;
-	for (const array<int, 3>& b : snowboots) {
-		while (tile_depth_index < sz(indices_by_depth) && tiles[indices_by_depth[tile_depth_index]] > b[0]) {
-			max_gap = max(max_gap, (*(next(indices.find(indices_by_depth[tile_depth_index]))) - *(prev(indices.find(indices_by_depth[tile_depth_index])))));
-			indices.erase(indices_by_depth[tile_depth_index]);
-			tile_depth_index++;
+	while (!boots.empty()) {
+		while (depths.top().first > boots.top()[0]) {
+			max_step = max(max_step, *(next(tiles.find(depths.top().second))) - *(prev(tiles.find(depths.top().second))));
+			tiles.erase(depths.top().second);
+			depths.pop();
 		}
 
-		if (max_gap > b[1]) {
-			answer_key[b[2]] = 0;
-		}
-		else {
-			answer_key[b[2]] = 1;
-		}
+		if (boots.top()[1] >= max_step) viable[boots.top()[2]] = 1;
+		boots.pop();
 	}
 
-	for (const bool& b : answer_key) {
-		cout << b << nl;
-	}
-	
+	for (bool v : viable) cout << v << "\n";
 	return 0;
 }
