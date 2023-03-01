c0c3127654c64b47836532f7cdd739ebbf9725ed
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 08:35:09 -0500

    Updates Files

--- USACO/Bronze/2015-2016/December/Speeding Ticket.cpp
+++ USACO/Bronze/2015-2016/December/Speeding Ticket.cpp
@@ -1,63 +1,48 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     freopen("speeding.in", "r", stdin);
     freopen("speeding.out", "w", stdout);
 
-    int N;
-    int M;
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    vector<pair<int, int>> N_segments;
-    vector<pair<int, int>> M_segments;
+    int N, M;
+    cin >> N >> M;
 
-    scanf("%d%d", &N, &M);
+    vector<pair<int, int>> SN(N);
+    for (pair<int, int> &sn : SN) cin >> sn.first >> sn.second;
 
-    for (int i = 0; i < N; i++) {
-        int segment_miles;
-        int segment_speed;
+    vector<pair<int, int>> SM(M);
+    for (pair<int, int> &sm : SM) cin >> sm.first >> sm.second;
 
-        scanf("%d%d", &segment_miles, &segment_speed);
-        N_segments.push_back({segment_miles, segment_speed});
-    }
-
-    for (int i = 0; i < M; i++) {
-        int segment_miles;
-        int segment_speed;
-
-        scanf("%d%d", &segment_miles, &segment_speed);
-        M_segments.push_back({segment_miles, segment_speed});
-    }
-
-    int max_infraction = 0;
+    int res = 0;
     int N_miles = 0;
     int M_miles = 0;
     int M_index = 0;
 
     for (int i = 0; i < N; i++) {
-        int N_speed = N_segments[i].second;
-        N_miles += N_segments[i].first;
+        int N_speed = SN[i].second;
+        N_miles += SN[i].first;
 
         while (N_miles > M_miles) {
-            int M_speed = M_segments[M_index].second;
+            int M_speed = SM[M_index].second;
             int speed_difference = M_speed - N_speed;
-            max_infraction = max(speed_difference, max_infraction);
+            res = max(speed_difference, res);
 
-            if (M_miles + M_segments[M_index].first > N_miles) {
-                int mile_difference = M_miles + M_segments[M_index].first - N_miles;
+            if (M_miles + SM[M_index].first > N_miles) {
+                int mile_difference = M_miles + SM[M_index].first - N_miles;
                 M_miles = N_miles;
-                M_segments[M_index].first = mile_difference;
+                SM[M_index].first = mile_difference;
             }
             else {
-                M_miles += M_segments[M_index].first;
+                M_miles += SM[M_index].first;
                 M_index++;
             }
         }
     }
 
-    printf("%d\n", max_infraction);
+    cout << res << "\n";
     return 0;
 }
