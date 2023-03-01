bd483663320887d8a96cf2c447983d67f17247ff
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 10:25:41 -0500

    Updated Files

--- USACO/Bronze/2021-2021/December/Daisy Chains.cpp
+++ USACO/Bronze/2021-2021/December/Daisy Chains.cpp
@@ -1,41 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define nl "\n"
-#define spc " "
-#define sz(x) ((int) x.size())
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int N; cin >> N;
+	vector<int> P(N); for (int &p : P) cin >> p;
 
-	vector<int> flowers(N + 1), petals(N + 1);
-	for (int i = 1; i <= N; i++) {
-		cin >> flowers[i];
-		petals[i] = petals[i - 1] + flowers[i];
-	}
+	int res = 0;
+	for (int i = 0; i < N; i++) {
+		int ret = 0;
 
-	int average_flowers = N;
-	for (int i = 1; i <= N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			int total_petals = petals[j] - petals[i - 1];
-			int total_flowers = j - i + 1;
-
-			if (total_petals % total_flowers == 0) {
-				int average_petals = total_petals / total_flowers;
-
-				for (int k = i; k <= j; k++) {
-					if (flowers[k] == average_petals) {
-						average_flowers++;
-						break;
-					}
-				}
+		for (int j = i; j < N; j++) {
+			ret += P[j]; int n = j - i + 1; if (ret % n) continue;
+			for (int k = i; k <= j; k++) {
+				if (P[k] == ret / n) { res++; break; }
 			}
 		}
 	}
 
-	cout << average_flowers << nl;
+	cout << res << "\n";
 	return 0;
 }
