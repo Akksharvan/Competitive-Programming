273b99a35270c2957e4edec933d10a763f103e16
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 10:00:32 -0500

    Updated Files

--- USACO/Bronze/2019-2020/US Open/Cowntact Tracing.cpp
+++ USACO/Bronze/2019-2020/US Open/Cowntact Tracing.cpp
@@ -1,60 +1,37 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-bool cow_ends_infected[101];
-int N, cowx[251], cowy[251];
- 
-bool consistent_with_data(int patient_zero, int K)
-{
-  bool infected[101] = {false};
-  int num_handshakes[101] = {0};
-  infected[patient_zero] = true;
-  for (int t=0; t<=250; t++) {
-    int x = cowx[t], y = cowy[t];
-    if (x>0) {
-      if (infected[x]) num_handshakes[x]++;
-      if (infected[y]) num_handshakes[y]++;
-      if (num_handshakes[x] <= K && infected[x]) infected[y] = true;
-      if (num_handshakes[y] <= K && infected[y]) infected[x] = true;
+
+int main() {
+    freopen("tracing.in", "r", stdin);
+    freopen("tracing.out", "w", stdout);
+
+    ios::sync_with_stdio(false);
+	  cin.tie(nullptr);
+
+    int N, T; string S;
+    cin >> N >> T >> S;
+
+    vector<bool> I(N); for (int i = 0; i < N; i++) I[i] = ((S[i] == '1') ? 1 : 0);
+    vector<array<int, 3>> H(T); for (array<int, 3> &h : H) cin >> h[0] >> h[1] >> h[2];
+
+    array<int, 3> res{0, INT_MAX, 0}; sort(begin(H), end(H));
+    for (int i = 0; i < N; i++) {
+        bool G = 0; if (!I[i]) continue;
+        for (int K = 0; K <= T; K++) {
+            vector<bool> B(N); B[i] = 1; vector<int> C(N + 1);
+            for (array<int, 3> &h : H) {
+                if (B[h[1] - 1]) C[h[1]]++;
+                if (B[h[2] - 1]) C[h[2]]++;
+                if (B[h[1] - 1] && C[h[1]] <= K) B[h[2] - 1] = 1;
+                if (B[h[2] - 1] && C[h[2]] <= K) B[h[1] - 1] = 1;
+            }
+
+            if (I == B) { G = 1; res[1] = min(res[1], K); res[2] = max(res[2], K); }
+        }
+
+        if (G) res[0]++;
     }
-  }
-  for (int i=1; i<=N; i++)
-    if (infected[i] != cow_ends_infected[i]) return false;
-  return true;
-}
- 
-int main(void)
-{
-  int T, t, x, y;
-  string s;
- 
-  ifstream fin ("tracing.in");
-  fin >> N >> T >> s;
-  for (int i=1; i<=N; i++)
-    cow_ends_infected[i] = s[i-1]=='1';
-  for (int i=0; i<T; i++) {
-    fin >> t >> x >> y;
-    cowx[t] = x;
-    cowy[t] = y;
-  }
- 
-  bool possible_i[101] = {false};
-  bool possible_K[252] = {false};
-  for (int i=1; i<=N; i++)
-    for (int K=0; K<=251; K++)
-      if (consistent_with_data(i, K)) 
-	possible_i[i] = true; possible_K[K] = true;
- 
-  int lower_K=251, upper_K=0, num_patient_zero=0;
-  for (int K=0; K<=251; K++) if (possible_K[K]) upper_K = K;
-  for (int K=251; K>=0; K--) if (possible_K[K]) lower_K = K;
-  for (int i=1; i<=N; i++) if (possible_i[i]) num_patient_zero++;
- 
-  ofstream fout ("tracing.out");
-  fout << num_patient_zero << " " << lower_K << " ";
-  if (upper_K==251) fout << "Infinity\n";
-  else fout << upper_K << "\n";
- 
-  return 0;
+
+    cout << res[0] << " " << res[1] << " " << ((res[2] == T) ? ("Infinity") : (to_string(res[2]))) << "\n";
+    return 0;
 }
