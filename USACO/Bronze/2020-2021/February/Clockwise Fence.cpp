a7bd42351a571e4730105f0561f8d5333bac31ac
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 11:02:04 -0500

    Updated Files

--- USACO/Bronze/2020-2021/February/Clockwise Fence.cpp
+++ USACO/Bronze/2020-2021/February/Clockwise Fence.cpp
@@ -1,84 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define nl "\n"
-#define spc " "
-#define sz(x) ((int) x.size())
 
-vector<pair<int, int>> vertices_from_fence_path(string fence_path) {
-	vector<pair<int, int>> vertices;
-	int x = 0, y = 0;
-	for (int i = 0; i < sz(fence_path); i++) {
-		if (i == 0) {
-			if (fence_path[i] != fence_path[sz(fence_path) - 1]) {
-				vertices.push_back({x, y});
-			}
-		}
-		else {
-			if (fence_path[i] != fence_path[i - 1]) {
-				vertices.push_back({x, y});
-			}
-		}
+vector<pair<int, int>> foo (string S) {
+	vector<pair<int, int>> V; int x = 0, y = 0;
+	
+	for (int i = 0; i < (int) S.size(); i++) {
+		if (i == 0 && S[i] != S[(int) S.size() - 1]) V.push_back({x, y});
+		else if (i != 0 && S[i] != S[i - 1]) V.push_back({x, y});
 
-		if (fence_path[i] == 'E') {
-			x++;
-		}
-		else if (fence_path[i] == 'N') {
-			y++;
-		}
-		else if (fence_path[i] == 'W') {
-			x--;
-		}
-		else if (fence_path[i] == 'S') {
-			y--;
-		}
+		if (S[i] == 'E') x++;
+		else if (S[i] == 'N') y++;
+		else if (S[i] == 'W') x--;
+		else if (S[i] == 'S') y--;
 	}
 
-	return vertices;
+	return V;
 }
 
-int shoelace (vector<pair<int, int>>& vertices) {
-	int component_one = 0, component_two = 0;
-	for (int i = 0; i < sz(vertices); i++) {
-		if (i != sz(vertices) - 1) {
-			component_one += (vertices[i].first * vertices[i + 1].second);
-		}
-		else {
-			component_one += (vertices[i].first * vertices[0].second);
-		}
+int SL (vector<pair<int, int>>& V) {
+	int ret1 = 0, ret2 = 0;
+
+	for (int i = 0; i < (int) V.size(); i++) {
+		if (i != (int) V.size() - 1) ret1 += (V[i].first * V[i + 1].second);
+		else ret1 += (V[i].first * V[0].second);
 	}
 
-	for (int i = 0; i < sz(vertices); i++) {
-		if (i != sz(vertices) - 1) {
-			component_two += (vertices[i].second * vertices[i + 1].first);
-		}
-		else {
-			component_two += (vertices[i].second * vertices[0].first);
-		}
+	for (int i = 0; i < (int) V.size(); i++) {
+		if (i != (int) V.size() - 1) ret2 += (V[i].second * V[i + 1].first);
+		else ret2 += (V[i].second * V[0].first);
 	}
 
-	return (component_one - component_two) / 2;
+	return (ret1 - ret2) / 2;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int N; cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		string fence_path;
-		cin >> fence_path;
+		string S; cin >> S;
+		vector<pair<int, int>> V = foo(S); int ret = SL(V);
 
-		vector<pair<int, int>> vertices = vertices_from_fence_path(fence_path);
-		int area = shoelace(vertices);
-
-		if (area > 0) {
-			cout << "CCW" << nl;
-		}
-		else {
-			cout << "CW" << nl;
-		}
+		if (ret > 0) cout << "CCW" << "\n";
+		else cout << "CW" << "\n";
 	}
 
 	return 0;
