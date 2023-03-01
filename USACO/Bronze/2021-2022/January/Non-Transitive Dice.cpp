662a42cb4f037967f3df80648530bb1f4431c3b2
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 11:56:08 -0500

    Updated Files

--- USACO/Bronze/2021-2022/January/Non-Transitive Dice.cpp
+++ USACO/Bronze/2021-2022/January/Non-Transitive Dice.cpp
@@ -1,74 +1,51 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-bool can_beat(vector<int> &die_one, vector<int> &die_two) {
-	int case_win = 0;
-	int case_loss = 0;
+bool foo (vector<int> &x, vector<int> &y) {
+	int ret1 = 0, ret2 = 0;
 
-	for (const int &roll_one : die_one) {
-		for (const int &roll_two : die_two) {
-			if (roll_one > roll_two) {
-				case_win++;
-			} else if (roll_one < roll_two) {
-				case_loss++;
-			}
+	for (const int &r1 : x) {
+		for (const int &r2 : y) {
+			if (r1 > r2) ret1++;
+			else if (r1 < r2) ret2++;
 		}
 	}
 
-	if (case_win > case_loss) {
-		return true;
-	} else {
-		return false;
-	}
+	if (ret1 > ret2) return 1;
+	else return 0;
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int T;
-	cin >> T;
-
-	vector<vector<int>> C_possibilities;
+void solve() {
+	vector<vector<int>> D;
 
 	for (int i = 1; i <= 10; i++) {
 		for (int j = 1; j <= 10; j++) {
 			for (int k = 1; k <= 10; k++) {
 				for (int l = 1; l <= 10; l++) {
-					C_possibilities.push_back({i, j, k, l});
+					D.push_back({i, j, k, l});
 				}
 			}
 		}
 	}
 
-	while (T--) {
-		bool possible = false;
+	vector<int> A(4), B(4);
+	cin >> A[0] >> A[1] >> A[2] >> A[3] >> B[0] >> B[1] >> B[2] >> B[3];
 
-		vector<int> A(4), B(4);
-		cin >> A[0] >> A[1] >> A[2] >> A[3] >> B[0] >> B[1] >> B[2] >> B[3];
-
-		if (can_beat(A, B) || can_beat(B, A)) {
-			if (can_beat(B, A)) {
-				vector<int> temporary_storage = B;
-				B = A;
-				A = temporary_storage;
-			}
-
-			for (vector<int> C : C_possibilities) {
-				if (can_beat(B, C) && can_beat(C, A)) {
-					possible = true;
-					break;
-				}
-			}
-		}
-
-		if (possible) {
-			cout << "yes" << "\n";
-		} else {
-			cout << "no" << "\n";
-		}
+	if (foo (A, B) || foo (B, A)) {
+		if (foo (B, A)) { vector<int> S = B; B = A; A = S; }
+		for (vector<int> &C : D) { if (foo (B, C) && foo (C, A)) { cout << "yes" << "\n"; return; } }
 	}
+	
+	cout << "no" << "\n";
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+
+	int T; cin >> T;
+	for (int t = 1; t <= T; t++) solve();
 
 	return 0;
 }
