4f8288f347e50be4a9e6d1d946e52514d42da8ee
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 08:30:09 -0500

    Updated Files

--- USACO/Bronze/2015-2016/February/Load Balancing.cpp
+++ USACO/Bronze/2015-2016/February/Load Balancing.cpp
@@ -1,55 +1,36 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <set>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 	freopen("balancing.in", "r", stdin);
 	freopen("balancing.out", "w", stdout);
 	
-	int cow_num;
-	int max_pos;
-	cin >> cow_num >> max_pos;
+	int N, B; cin >> N >> B;
+	vector<int> X(N), Y(N);
+	set<int> V, H;
 
-	vector<int> x_vals(cow_num);
-	vector<int> y_vals(cow_num);
-	set<int> vfence;
-	set<int> hfence;
-	for (int c = 0; c < cow_num; c++) {
-		cin >> x_vals[c] >> y_vals[c];
-		vfence.insert(x_vals[c] + 1);
-		hfence.insert(y_vals[c] + 1);
+	for (int i = 0; i < N; i++) {
+		cin >> X[i] >> Y[i];
+		V.insert(X[i] + 1);
+		H.insert(Y[i] + 1);
 	}
 	
-	int min_imbalance = cow_num;
-	for (int v : vfence) {
-		for (int h : hfence) {
-			int top_left = 0;
-			int top_right = 0;
-			int bottom_left = 0;
-			int bottom_right = 0;
+	int res = N;
+	for (const int v : V) {
+		for (const int h : H) {
+			int tl = 0, tr = 0;
+			int bl = 0, br = 0;
 			
-			for (int c = 0; c < cow_num; c++) {
-				if (x_vals[c] < v && y_vals[c] > h) {
-					top_left++;
-				} else if (x_vals[c] > v && y_vals[c] > h) {
-					top_right++;
-				} else if (x_vals[c] < v && y_vals[c] < h) {
-					bottom_left++;
-				} else if (x_vals[c] > v && y_vals[c] < h){
-					bottom_right++;
-				}
+			for (int i = 0; i < N; i++) {
+                if (X[i] < v && Y[i] > h) tl++;
+                else if (X[i] > v && Y[i] > h) tr++;
+                else if (X[i] < v && Y[i] < h) bl++;
+                else if (X[i] > v && Y[i] < h) br++;
 			}
 			
-			min_imbalance = min(
-				min_imbalance,
-				max({top_left, top_right, bottom_left, bottom_right})
-			);
+			res = min(res, max({tl, tr, bl, br}));
 		}
 	}
 
-	cout << min_imbalance << endl;
+	cout << res << endl;
 }
