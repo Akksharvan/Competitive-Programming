7cba864e79f48aa2937d4076dcb5710d56f32f14
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 10:04:54 -0500

    Updated Files

--- USACO/Bronze/2021-2021/December/Do You Know Your ABCs?.cpp
+++ USACO/Bronze/2021-2021/December/Do You Know Your ABCs?.cpp
@@ -1,26 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define nl "\n"
-#define spc " "
-#define sz(x) ((int) x.size())
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int A, B, C;
-	array<int, 7> integers;
+	int A, B, C; array<int, 7> D;
+    for (int &d : D) cin >> d;
 
-	for (int i = 0; i < 7; i++) {
-		cin >> integers[i];
-	}
-	sort(begin(integers), end(integers));
+	sort(begin(D), end(D)); A = D[0], B = D[1]; C = D[6] - (D[0] + D[1]);
+	cout << A << " " << B << " " << C << "\n";
 
-	A = integers[0];
-	B = integers[1];
-	C = integers[6] - (integers[0] + integers[1]);
-
-
-	cout << A << spc << B << spc << C << nl;
 	return 0;
 }
