69b77e6088bc49c78f397676e6fc0a3a623fb756
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-01 11:47:20 -0500

    Updated Files

--- USACO/Bronze/2021-2022/January/Herdle.cpp
+++ USACO/Bronze/2021-2022/January/Herdle.cpp
@@ -1,49 +1,31 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	vector<vector<char>> answer_key(3);
-	multiset<int> answer_count;
-	multiset<int> guess_count;
-
-	int green_squares = 0;
-	int yellow_squares = 0;
+	vector<vector<char>> A(3);
+	multiset<int> AC, BC; int GS = 0, YS = 0;
 
 	for (int i = 0; i < 3; i++) {
-		string s;
-		cin >> s;
-
-		for (int j = 0; j < 3; j++) {
-			answer_key[i].push_back(s[j]);
-			answer_count.insert(s[j]);
-		}
+		string s; cin >> s;
+		for (int j = 0; j < 3; j++) A[i].push_back(s[j]), AC.insert(s[j]);
 	}
 
 	for (int i = 0; i < 3; i++) {
-		string s;
-		cin >> s;
+		string s; cin >> s;
 
 		for (int j = 0; j < 3; j++) {
-			if (s[j] == answer_key[i][j]) {
-				answer_count.erase(answer_count.find(s[j]));
-				green_squares++;
-			} else {
-				guess_count.insert(s[j]);
-			}
+			if (s[j] == A[i][j]) AC.erase(AC.find(s[j])), GS++;
+			else BC.insert(s[j]);
 		}
 	}
 
-	for (char c : guess_count) {
-		if (answer_count.count(c) > 0) {
-			answer_count.erase(answer_count.find(c));
-			yellow_squares++;
-		}
+	for (char c : BC) {
+		if (AC.count(c) > 0) AC.erase(AC.find(c)), YS++;
 	}
 
-	cout << green_squares << "\n" << yellow_squares << "\n";
+	cout << GS << "\n" << YS << "\n";
 	return 0;
 }
