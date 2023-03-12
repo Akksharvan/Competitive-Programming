b4c2453be45939ec03d52631b61756de84287ef5
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-11 23:45:47 -0500

    Added Files

---
+++ Kattis/Basketball One on One.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	string s; cin >> s;
+	cout << s[(int) s.size() - 2] << "\n";
+
+	return 0;
+}
