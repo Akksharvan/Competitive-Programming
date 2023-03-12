0f918076eda65fabda938d36110c2c8831d767a9
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-11 20:53:56 -0500

    Added Files

--- USACO/Bronze/2017-2018/January/Blocked Billboard II.cpp
+++ USACO/Bronze/2017-2018/January/Blocked Billboard II.cpp
@@ -5,12 +5,12 @@ int main() {
 	freopen("billboard.in", "r", stdin);
 	freopen("billboard.out", "w", stdout);
 
-	vector<int> A(4), B(4); for (int i = 0; i < 3; i++) { cin >> A[i] >> B[i]; }
-	
-	if (A[3] >= A[1] && A[2] <= A[0] && B[3] >= B[1] && B[2] <= B[0]) cout << 0;
-	else if (A[2] <= A[0] && B[2] <= B[0] && B[3] > B[0] && A[3] >= A[1]) cout << (A[1] - A[0]) * (B[1] - B[3]);
-	else if (B[2] < B[1] && A[2] <= A[0] && B[3] >= B[1] && A[3] >= A[1]) cout << (A[1] - A[0]) * (B[2] - B[0]);
-	else if (A[3] > A[0] && A[2] <= A[0] && B[3] >= B[1] && B[2] <= B[0]) cout << (A[1] - A[3]) * (B[1] - B[0]);
-	else if (A[2] < A[1] && A[3] >= A[1] && B[3] >= B[1] && B[2] <= A[0]) cout << (A[2] - A[0]) * (B[1] - B[0]);
-	else cout << (A[1] - A[0]) * (B[1] - B[0]); return 0;
+	vector<int> A(5), B(5); for (int i = 1; i <= 4; i++) { cin >> A[i] >> B[i]; }
+
+	if (A[4] >= A[2] && A[3] <= A[1] && B[4] >= B[2] && B[3] <= B[1]) cout << 0;
+	else if (A[3] <= A[1] && B[3] <= B[1] && B[4] > B[1] && A[4] >= A[2]) cout << (A[2] - A[1]) * (B[2] - B[4]);
+	else if (B[3] < B[2] && A[3] <= A[1] && B[4] >= B[2] && A[4] >= A[2]) cout << (A[2] - A[1]) * (B[3] - B[1]);
+	else if (A[4] > A[1] && A[3] <= A[1] && B[4] >= B[2] && B[3] <= B[1]) cout << (A[2] - A[4]) * (B[2] - B[1]);
+	else if (A[3] < A[2] && A[4] >= A[2] && B[4] >= B[2] && B[3] <= A[1]) cout << (A[3] - A[1]) * (B[2] - B[1]);
+	else cout << (A[2] - A[1]) * (B[2] - B[1]); return 0;
 }
