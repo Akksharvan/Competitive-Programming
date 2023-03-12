d3c730f69aa05b03195b4dc04ae9470969f2d59f
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2023-03-11 21:16:50 -0500

    Updated Files

--- Codeforces/B - Two Tables.cpp
+++ Codeforces/B - Two Tables.cpp
@@ -1,66 +1,24 @@
-#include <iostream>
-#include <algorithm>
-#include <cstdint>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+	int W, H, w, h, x1, y1, x2, y2;
+	cin >> W >> H >> x1 >> y1 >> x2 >> y2 >> w >> h;
+
+	int res = INT_MAX;
+	if (x2 - x1 + w <= W) { res = min(res, max(0, w - x1)); res = min(res, max(0, x2 - (W - w))); }
+	if (y2 - y1 + h <= H) { res = min(res, max(0, h - y1)); res = min(res, max(0, y2 - (H - h))); }
+
+	if (res == INT_MAX) cout << -1 << "\n";
+	else cout << (double) (res) << "\n"; return;
+}
+
 int main() {
-	int t;
-	cin >> t;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-	while (t--) {
-		int W, H, x1, y1, x2, y2, w, h, x3, y3, x4, y4;
-		cin >> W >> H >> x1 >> y1 >> x2 >> y2 >> w >> h;
-
-		int minimum_distance = INT32_MAX;
-		bool possible = false;
-
-		if (y2 + (h - y1) <= H) {
-			possible = true;
-			if (h - y1 <= 0) {
-				minimum_distance = 0;
-			}
-			else {
-				minimum_distance = min(minimum_distance, h - y1);
-			}
-		}
-
-		if (y1 - (y2 - (H - h)) >= 0) {
-			possible = true;
-			if ((H - h) >= y2) {
-				minimum_distance = 0;
-			}
-			else {
-				minimum_distance = min(minimum_distance, y2 - (H - h));
-			}
-		}
-
-		if (x2 + (w - x1) <= W) {
-			possible = true;
-			if (w - x1 <= 0) {
-				minimum_distance = 0;
-			}
-			else {
-				minimum_distance = min(minimum_distance, w - x1);
-			}
-		}
-
-		if (x1 - (x2 - (W - w)) >= 0) {
-			possible = true;
-			if ((W - w) >= x2) {
-				minimum_distance = 0;
-			}
-			else {
-				minimum_distance = min(minimum_distance, x2 - (W - w));
-			}
-		}
-
-		if (!possible) {
-			minimum_distance = -1;
-		}
-
-		cout << minimum_distance << "\n";
-	}
+	int T; cin >> T; cout << fixed << setprecision(9);
+	for (int t = 1; t <= T; t++) solve();
 
 	return 0;
 }
